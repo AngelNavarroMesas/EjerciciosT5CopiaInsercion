cad2035c6e139334d168f52d2b2b95e72df78cf1
anavarro <anavarro@106-30.info2>
2023-01-17 12:39:47 +0100

    el cuatro no termina de funcionar

--- EjerciciosT5CopiaElementos/src/ejercicios/Ejercicio4.java
+++ EjerciciosT5CopiaElementos/src/ejercicios/Ejercicio4.java
@@ -1,8 +1,31 @@
 package ejercicios;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Ejercicio4 {
 	
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int tablaEnteros[] = new int[10];
+		int tabla[] = new int[0];
+		
+		for(int i=0;i!=tablaEnteros.length;i++) {
+			System.out.println("Introduzca un numero");
+			tablaEnteros[i] = sc.nextInt();
+		}
+		Arrays.sort(tablaEnteros);
+		for(int i=0;i!=tablaEnteros[tablaEnteros.length-1];i++) {
+			int p = Arrays.binarySearch(tablaEnteros, i);
+			if(p>=0) {
+				tabla = Arrays.copyOf(tabla, tabla.length+1);
+				System.arraycopy(tablaEnteros, i, tabla, tabla.length-1, 1);
+			}
+		}
+		
+		System.out.println(Arrays.toString(tablaEnteros));
+		System.err.println(Arrays.toString(tabla));
 		
 	}
 }
